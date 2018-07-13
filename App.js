c2f77e14c87dff48df26e09066ebbe453f320f69
Noel Escobedo <noel_ska15@hotmail.com>
2018-07-12 22:36:25 -0500

    New initial app using expo

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-import React, {Component} from 'react';
-import ExternalRoutes from './routes/ExternalRoutes';
-
-export default class App extends Component {
+export default class App extends React.Component {
   render() {
     return (
-      <ExternalRoutes />
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- routes/ExternalRoutes.js
+++
@@ -1,8 +0,0 @@
-import { createStackNavigator } from 'react-navigation';
-import Home from '../components/Home';
-
-export default createStackNavigator({
-  Home: {
-    screen: Home
-  },
-});
