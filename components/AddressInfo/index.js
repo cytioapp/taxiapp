42f2b9bc17e967794166e3e5134cd9bc29fee20a
Salvador Jimenez Mendoza <salvador@michelada.io>
2018-07-13 11:40:21 -0500

    Removing unecessary comments and stuffs

--- components/AddressInfo/index.js
+++ components/AddressInfo/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import { View, Text, TextInput, StyleSheet } from 'react-native';
 import { Container, Header, Content, Form, Item, Input, Button, Text, Label, Icon } from 'native-base';
 
 export default class AddressInfo extends Component{
