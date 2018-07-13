edacadc04c98d215421f134e9a3cddd6ec0798a3
Salvador Jimenez Mendoza <salvador@michelada.io>
2018-07-13 17:31:54 -0500

    adding styles 2.0

--- components/Login/index.js
+++ components/Login/index.js
@@ -1,34 +1,50 @@
 import React, { Component } from 'react';
-import {StyleSheet, View} from 'react-native';
-import { Container, Header, Content, Form, Item, Input, Button, Text } from 'native-base';
-
+import {StyleSheet, View, KeyboardAvoidingView, Image} from 'react-native';
+import { Container, Content, Form, Item, Input, Button, Text } from 'native-base';
+import logoImage from '../../assets/taxi1.png';
 const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: 'center'
+ },
+  form: {
+    flex: 1,
+  },
   buttonWrapper: {
-    padding: 10
+    padding: 10,
+  },
+  imageWrapper: {
+    paddingTop: 50,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
-})
+});
 
 export default class Login extends Component{
   render(){
     return(
-      <Container>
-        <Header />
-        <Content>
-          <Form>
-            <Item>
-              <Input placeholder="Username" />
-            </Item>
-            <Item last>
-              <Input placeholder="Password" />
-            </Item>
+      <KeyboardAvoidingView style={{flex: 1}} behavior="padding">
+        <Container style={styles.container}>
+          <Content contentContainerStyle={{ flex: 1 }}>
+            <View style={styles.imageWrapper}>
+              <Image source={logoImage} style={{height: 100, width: 100}}/>
+            </View>
+            <Form styles={styles.form}>
+              <Item>
+                <Input placeholder="Usuario" />
+              </Item>
+              <Item last>
+                <Input placeholder="Contraseña" />
+              </Item>
+            </Form>
             <View style={styles.buttonWrapper} >
               <Button block rounded success onPress={() => this.props.navigation.navigate('Home')}>
                 <Text>Iniciar Sesión</Text>
               </Button>
             </View>
-          </Form>
-        </Content>
-      </Container>
+          </Content>
+        </Container>
+      </KeyboardAvoidingView>
     )
   }
 }
