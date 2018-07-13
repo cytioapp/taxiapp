a0cf2fd4b483acde0fa25b0af4341c3f63f7c001
Noel Escobedo <noel_ska15@hotmail.com>
2018-07-12 23:19:08 -0500

    Added google maps for android

--- App.js
+++ App.js
@@ -1,14 +1,67 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Constants, MapView, Location, Permissions } from 'expo';
 
 export default class App extends React.Component {
+  state = {
+    mapRegion: null,
+    hasLocationPermissions: false,
+    locationResult: null
+  };
+
+  componentDidMount() {
+    this._getLocationAsync();
+  }
+
+  _handleMapRegionChange = mapRegion => {
+    // console.log(mapRegion);
+    this.setState({ mapRegion });
+  };
+
+  _getLocationAsync = async () => {
+   let { status } = await Permissions.askAsync(Permissions.LOCATION);
+   if (status !== 'granted') {
+     this.setState({
+       locationResult: 'Permission to access location was denied',
+     });
+   } else {
+     this.setState({ hasLocationPermissions: true });
+   }
+
+   let location = await Location.getCurrentPositionAsync({});
+   // this.setState({ locationResult: JSON.stringify(location) });
+   this.setState({ locationResult: true });
+   
+   // Center the map on the location we just fetched.
+    this.setState({mapRegion: { latitude: location.coords.latitude, longitude: location.coords.longitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421 }});
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={styles.paragraph}>
+          Pan, zoom, and tap on the map!
+        </Text>
+        
+        {
+          this.state.locationResult === null ?
+          <Text>Finding your current location...</Text> :
+          this.state.hasLocationPermissions === false ?
+            <Text>Location permissions are not granted.</Text> :
+            this.state.mapRegion === null ?
+            <Text>Map region doesn't exist.</Text> :
+            <MapView
+              style={{ alignSelf: 'stretch', height: 400 }}
+              region={this.state.mapRegion}
+              onRegionChange={this._handleMapRegionChange}
+            />
+        }
+        
+        <Text>
+          Location: {this.state.locationResult}
+        </Text>
       </View>
+        
     );
   }
 }
@@ -16,8 +69,16 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e',
   },
 });
