b4b0de4e118862957ea45b621015d4dd5e0e4d68
Myrka Larios <myrka@michelada.io>
2018-08-25 22:26:10 -0500

    Adds validation to edit inputs

--- components/EditName/index.js
+++ components/EditName/index.js
@@ -8,73 +8,104 @@ import {
   Input,
   Left,
   Right,
-  Title,
+  Title
 } from 'native-base';
 import styles from './style';
 import Api from '../../utils/api';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import Modal from '../Modal';
 
 export default class EditEmail extends Component {
   state = {
     original_full_name: '',
     new_full_name: '',
-    buttonDisabled: true
-  }
+    buttonDisabled: true,
+    errors: [],
+    modalVisible: false
+  };
 
-  componentDidMount(){
+  componentDidMount() {
     this.fillFields();
   }
 
   fillFields = () => {
     Api.get('/users/profile')
-    .then(res => {
-      this.setState({
-        original_full_name: res.data.full_name,
-        new_full_name: res.data.full_name
-      });
-      this.difference();
-    }).catch(err => console.log(err))
-  }
+      .then(res => {
+        this.setState({
+          original_full_name: res.data.full_name,
+          new_full_name: res.data.full_name
+        });
+        this.difference();
+      })
+      .catch(err => console.log(err));
+  };
 
   handleReturn = () => {
-    if(this.difference() === true){
+    if (this.difference() === true) {
       Alert.alert(
         'Cambios sin guardar',
         '¿Guardar y salir?',
-        [
-          {text: 'No'},
-          {text: 'Si', onPress: () => this.handleSave()},
-        ],
+        [{ text: 'No' }, { text: 'Si', onPress: () => this.handleSave() }],
         { cancelable: false }
       );
     } else {
-      this.props.navigation.navigate('Profile')
+      this.props.navigation.navigate('Profile');
     }
-  }
+  };
 
   handleSave = () => {
-    Api.put('/users/profile', {full_name: this.state.new_full_name}).then( () => {
-      this.props.navigation.navigate('Profile')
-    });
-  }
+    if (this.validatesName(this.state.new_full_name)) {
+      Api.put('/users/profile', { full_name: this.state.new_full_name }).then(
+        () => {
+          this.props.navigation.navigate('Profile');
+        }
+      );
+    } else {
+      this.setState({
+        errors: ['Nombre inválido'],
+        modalVisible: true
+      });
+    }
+  };
 
   difference = () => {
-    if(this.state.original_full_name !== this.state.new_full_name){
-      this.setState({buttonDisabled: false})
-      return true
+    if (this.state.original_full_name !== this.state.new_full_name) {
+      this.setState({ buttonDisabled: false });
+      return true;
     } else {
-      this.setState({buttonDisabled: true})
-      return false
+      this.setState({ buttonDisabled: true });
+      return false;
     }
-  }
+  };
 
-  render(){
-    return(
+  validatesName = name => {
+    const regex = /^[áÁéÉíÍóÓúÚñÑa-z ,\-']+$/i;
+    return name.match(regex);
+  };
+
+  setModalVisible = visible => {
+    this.setState({
+      modalVisible: visible,
+      errors: visible ? this.state.errors : []
+    });
+  };
+
+  render() {
+    return (
       <KeyboardAwareScrollView style={styles.keyboard}>
-        <Header style={styles.header} iosBarStyle="light-content" androidStatusBarColor="#262626">
+        <Modal
+          errors={this.state.errors}
+          modalVisible={this.state.modalVisible}
+          setModalVisible={this.setModalVisible}
+        />
+        <Header
+          style={styles.header}
+          iosBarStyle="light-content"
+          androidStatusBarColor="#262626"
+        >
           <Left style={styles.leftHeader}>
             <Button transparent onPress={() => this.handleReturn()}>
-              <Icon name='ios-arrow-back' style={styles.menuIcon} />
+              <Icon name="ios-arrow-back" style={styles.menuIcon} />
             </Button>
           </Left>
           <Body style={styles.bodyHeader}>
@@ -90,8 +121,8 @@ export default class EditEmail extends Component {
               <Input
                 placeholder="Nombre completo"
                 autoCapitalize="none"
-                onChangeText= {new_full_name => {
-                  this.setState({ new_full_name }, () => this.difference())
+                onChangeText={new_full_name => {
+                  this.setState({ new_full_name }, () => this.difference());
                 }}
                 value={this.state.new_full_name}
                 placeholderTextColor="#5C5C5C"
@@ -100,12 +131,15 @@ export default class EditEmail extends Component {
             </View>
           </View>
 
-
-          <View style={styles.buttonWrapper} >
+          <View style={styles.buttonWrapper}>
             <Button
               block
-              style={ this.state.buttonDisabled ? styles.buttonDisabled : styles.button }
-              disabled = {this.state.buttonDisabled}
+              style={
+                this.state.buttonDisabled
+                  ? styles.buttonDisabled
+                  : styles.button
+              }
+              disabled={this.state.buttonDisabled}
               onPress={() => this.handleSave()}
             >
               <Text style={styles.buttonText}>Guardar</Text>
@@ -113,6 +147,6 @@ export default class EditEmail extends Component {
           </View>
         </View>
       </KeyboardAwareScrollView>
-    )
+    );
   }
 }
