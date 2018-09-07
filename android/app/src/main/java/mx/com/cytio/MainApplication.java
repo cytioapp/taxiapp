5871d0d15d1ea422829df8c5eb781c10b67b39e5
Noel Escobedo <noel@michelada.io>
2018-09-07 07:04:26 +0000

    Fixed firebase database implementation

--- android/app/src/main/java/mx/com/cytio/MainApplication.java
+++ android/app/src/main/java/mx/com/cytio/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.database.RNFirebaseDatabasePackage;
 import com.react.rnspinkit.RNSpinkitPackage;
 import br.com.classapp.RNSensitiveInfo.RNSensitiveInfoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -31,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new RNFirebasePackage(),
+            new RNFirebaseDatabasePackage(),
             new RNSpinkitPackage(),
             new RNSensitiveInfoPackage(),
             new VectorIconsPackage(),
