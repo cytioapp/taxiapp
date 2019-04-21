4561e16d66fcd2d344f979ac5f9cee346c8e6c8e
Paco Chacon <pacochacon94@gmail.com>
2019-04-21 06:29:57 -0500

    Listen to trip state in Internal routes

--- App.js
+++ App.js
@@ -3,19 +3,20 @@ import ExternalRoutes from './routes/ExternalRoutes';
 import InternalRoutes from './routes/InternalRoutes';
 import { Provider, Subscribe } from 'unstated';
 import sessionState from './states/session';
+import tripState from './states/trip';
 import Loading from './components/Loading';
 
 class App extends Component {
   render() {
     return (
       <Provider>
-        <Subscribe to={[sessionState]}>
-          {(session) => {
+        <Subscribe to={[sessionState, tripState]}>
+          {(session, trip) => {
             if (session.state.isLogued == null) {
               session.verify();
               return <Loading />;
             } else if (session.state.isLogued) {
-              return <InternalRoutes screenProps={{ session }}  />
+              return <InternalRoutes screenProps={{ session, trip }}  />
             } else {
               return <ExternalRoutes screenProps={{ session }} />
             }
